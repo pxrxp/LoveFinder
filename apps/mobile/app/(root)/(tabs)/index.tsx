542f8e4e8571ae96b452725adcc752f0b6d54e79
pxrxp <187556078+pxrxp@users.noreply.github.com>
2026-02-19 22:15:08 +0545

    Move horizontal and vertical guestures into Card component + Reconcile their conflict

--- apps/mobile/app/(root)/(tabs)/index.tsx
+++ apps/mobile/app/(root)/(tabs)/index.tsx
@@ -1,12 +1,8 @@
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import {
   useSharedValue,
-  useAnimatedStyle,
-  withSpring,
-  withTiming,
 } from "react-native-reanimated";
 import { useEffect, useState } from "react";
 
@@ -14,18 +10,12 @@ import { useFetch } from "@/hooks/useFetch";
 import { FeedUser } from "@/types/FeedUser";
 import Card from "@/components/Card";
 import DataLoader from "@/components/DataLoader";
-import { scheduleOnRN } from "react-native-worklets";
-import { showThemedError } from "@/services/themed-error";
-import { useTheme } from "@/contexts/ThemeContext";
-import { swipeUser } from "@/services/user-actions";
 
-const SCREEN_WIDTH = require("react-native").Dimensions.get("window").width;
 type SwipeStatus = "dislike" | "like" | "pending";
 
 export default function HomeScreen() {
   const { data, loading, error, refetch } = useFetch<FeedUser[]>("feed");
   const tabBarHeight = useBottomTabBarHeight();
-  const {themeColors} = useTheme();
 
   const x = useSharedValue(0);
   const y = useSharedValue(0);
@@ -59,66 +49,6 @@ export default function HomeScreen() {
     status.value = "pending";
   };
 
-  const postSwipe = async (receiver_id: string, type: "like" | "dislike") => {
-    try {
-      await swipeUser(receiver_id, type)
-    } catch (err) {
-      showThemedError(`Swipe POST failed:\n ${err}`, themeColors);
-    }
-  };
-
-  const gesture = Gesture.Pan()
-    .enabled(!bioExpanded)
-    .onUpdate((event) => {
-      x.value = event.translationX;
-      y.value = event.translationY * 0.1;
-      const swipePercent = Math.abs(x.value) / SCREEN_WIDTH;
-      status.value =
-        swipePercent >= 0.1 ? (x.value > 0 ? "like" : "dislike") : "pending";
-    })
-    .onEnd(() => {
-      const swipePercent = Math.abs(x.value) / SCREEN_WIDTH;
-      const topCard = cards[cards.length - 1];
-      const topCardUserID = topCard.user_id;
-      const currentStatus = status.value;
-      if (!topCard) return;
-
-      if (swipePercent >= 0.1) {
-        scheduleOnRN(swapTopCard);
-
-        x.value = withTiming(
-          Math.sign(x.value) * SCREEN_WIDTH * 1.5,
-          {},
-          (finished) => {
-            if (finished) {
-              if (currentStatus !== "pending")
-                scheduleOnRN(postSwipe, topCardUserID, currentStatus);
-              scheduleOnRN(removeTopCard);
-            }
-          },
-        );
-      } else {
-        status.value = "pending";
-        x.value = withSpring(0);
-        y.value = withSpring(0);
-      }
-    });
-
-  const style = useAnimatedStyle(() => ({
-    transform: [
-      { translateX: x.value },
-      { translateY: 2 * y.value },
-      { rotateZ: `${-0.17 * x.value}deg` },
-    ],
-  }));
-
-  const showHeartStyle = useAnimatedStyle(() => ({
-    opacity: status.value === "like" ? 1 : 0,
-  }));
-  const showCrossStyle = useAnimatedStyle(() => ({
-    opacity: status.value === "dislike" ? 1 : 0,
-  }));
-
   return (
     <SafeAreaView
       className="flex-1 px-5 pt-2 pb-1 bg-bgPrimaryLight dark:bg-bgPrimaryDark"
@@ -132,20 +62,18 @@ export default function HomeScreen() {
               {cards.map((item, index) => {
                 const isTop = index === cards.length - 1;
                 return (
-                  <GestureDetector
-                    key={item.user_id}
-                    gesture={isTop ? gesture : Gesture.Native()}
-                  >
                     <Card
-                      style={style}
-                      showHeartStyle={showHeartStyle}
-                      showCrossStyle={showCrossStyle}
+                      key={item.user_id}
+                      x={x}
+                      y={y}
+                      status={status}
                       item={item}
                       isTop={isTop}
                       bioExpanded={bioExpanded}
                       setBioExpanded={setBioExpanded}
+                      swapTopCard={swapTopCard}
+                      removeTopCard={removeTopCard}
                     />
-                  </GestureDetector>
                 );
               })}
             </View>
--- apps/mobile/components/Card.tsx
+++ apps/mobile/components/Card.tsx
@@ -8,6 +8,7 @@ import Animated, {
   SlideInDown,
   SlideOutDown,
   useAnimatedScrollHandler,
+  SharedValue,
 } from "react-native-reanimated";
 import { LinearGradient } from "expo-linear-gradient";
 import MaskedView from "@react-native-masked-view/masked-view";
@@ -24,12 +25,15 @@ import { useEffect, useState } from "react";
 import { GestureDetector, Gesture } from "react-native-gesture-handler";
 import { Image } from "expo-image";
 import ReportModal from "./modals/ReportModal";
-import { reportUser } from "@/services/user-actions";
+import { reportUser, swipeUser } from "@/services/user-actions";
 import { showThemedError } from "@/services/themed-error";
 import { showThemedSuccess } from "@/services/themed-success";
 import { ImageCarousel } from "./ImageCarousel";
 import { scheduleOnRN } from "react-native-worklets";
 import { useFetch } from "@/hooks/useFetch";
+
+const SCREEN_WIDTH = require("react-native").Dimensions.get("window").width;
+type SwipeStatus = "dislike" | "like" | "pending";
 
 interface Photo {
   photo_id: string;
@@ -39,23 +43,27 @@ interface Photo {
 }
 
 interface CardProps {
-  style: any;
-  showHeartStyle: any;
-  showCrossStyle: any;
   item: FeedUser;
   isTop: boolean;
   bioExpanded: boolean;
   setBioExpanded: React.Dispatch<React.SetStateAction<boolean>>;
+  x: SharedValue<number>;
+  y: SharedValue<number>;
+  status: SharedValue<SwipeStatus>;
+  removeTopCard: () => void;
+  swapTopCard: () => void;
 }
 
 export default function Card({
-  style,
-  showHeartStyle,
-  showCrossStyle,
   item,
   isTop,
   bioExpanded,
   setBioExpanded,
+  x,
+  y,
+  status,
+  removeTopCard,
+  swapTopCard,
 }: CardProps) {
   const { themeColors } = useTheme();
   const [reportMenuVisible, setReportMenuVisible] = useState(false);
@@ -66,19 +74,38 @@ export default function Card({
     `photos/${cleanUserId}`,
   );
   const [photos, setPhotos] = useState<string[]>([item.image_url]);
+
+  const postSwipe = async (receiver_id: string, type: "like" | "dislike") => {
+    try {
+      await swipeUser(receiver_id, type);
+    } catch (err) {
+      showThemedError(`Swipe POST failed:\n ${err}`, themeColors);
+    }
+  };
+
   useEffect(() => {
-    if (userPhotos && userPhotos.length > 0) {
-      const photoUrls = userPhotos.map((p) => p.image_url);
-      setPhotos(photoUrls);
-    }
-    if (error) {
-      showThemedError(`Failed to fetch photos\n${error}`, themeColors);
-    }
+    if (userPhotos?.length) setPhotos(userPhotos.map((p) => p.image_url));
+    if (error) showThemedError(`Failed to fetch photos\n${error}`, themeColors);
   }, [userPhotos, error]);
 
   const scrollY = useSharedValue(0);
   const gestureStartY = useSharedValue(0);
   const expandActivationY = useSharedValue(0);
+
+  const style = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: x.value },
+      { translateY: 2 * y.value },
+      { rotateZ: `${-0.17 * x.value}deg` },
+    ],
+  }));
+
+  const showHeartStyle = useAnimatedStyle(() => ({
+    opacity: status.value === "like" ? 1 : 0,
+  }));
+  const showCrossStyle = useAnimatedStyle(() => ({
+    opacity: status.value === "dislike" ? 1 : 0,
+  }));
 
   const scrollHandler = useAnimatedScrollHandler({
     onScroll: (event) => {
@@ -86,9 +113,7 @@ export default function Card({
     },
   });
 
-  // -- Animations --
   const rotateY = useSharedValue(0);
-
   const wobble = (direction: "left" | "right") => {
     const deg = direction === "left" ? 10 : -10;
     rotateY.value = withSequence(
@@ -96,6 +121,10 @@ export default function Card({
       withSpring(0, { damping: 40, stiffness: 1200 }),
     );
   };
+
+  const animatedWobble = useAnimatedStyle(() => ({
+    transform: [{ perspective: 1000 }, { rotateY: `${rotateY.value}deg` }],
+  }));
 
   const verticalSwipe = Gesture.Pan()
     .activeOffsetY([-15, 15])
@@ -106,29 +135,58 @@ export default function Card({
     .onEnd((event) => {
       const swipeUp = event.translationY < -80;
       const swipeDown = event.translationY > 80;
-
       const startedInBottomArea =
         gestureStartY.value >= expandActivationY.value;
 
-      // Expand only if swipe started in bottom panel
       if (swipeUp && !bioExpanded && startedInBottomArea) {
         scheduleOnRN(setBioExpanded, true);
         return;
       }
 
-      // Close only if scroll at top
       if (swipeDown && bioExpanded && scrollY.value <= 0) {
         scheduleOnRN(setBioExpanded, false);
       }
     });
 
-  const animatedWobble = useAnimatedStyle(() => ({
-    transform: [{ perspective: 1000 }, { rotateY: `${rotateY.value}deg` }],
-  }));
+  const horizontalSwipe = Gesture.Pan()
+    .enabled(isTop && !bioExpanded)
+    .activeOffsetX([-10, 10])
+    .failOffsetY([-15, 15])
+    .onUpdate((event) => {
+      x.value = event.translationX;
+      y.value = event.translationY * 0.1;
+      const swipePercent = Math.abs(x.value) / SCREEN_WIDTH;
+      status.value =
+        swipePercent >= 0.1 ? (x.value > 0 ? "like" : "dislike") : "pending";
+    })
+    .onEnd(() => {
+      const swipePercent = Math.abs(x.value) / SCREEN_WIDTH;
+      const currentStatus = status.value;
 
-  if (photos.length === 0) {
-    return null;
-  }
+      if (swipePercent >= 0.1) {
+        scheduleOnRN(swapTopCard);
+
+        x.value = withTiming(
+          Math.sign(x.value) * SCREEN_WIDTH * 1.5,
+          {},
+          (finished) => {
+            if (finished) {
+              if (currentStatus !== "pending")
+                scheduleOnRN(postSwipe, item.user_id, currentStatus);
+              scheduleOnRN(removeTopCard);
+            }
+          },
+        );
+      } else {
+        status.value = "pending";
+        x.value = withSpring(0);
+        y.value = withSpring(0);
+      }
+    });
+
+  const combinedGesture = Gesture.Race(verticalSwipe, horizontalSwipe);
+
+  if (photos.length === 0) return null;
 
   return (
     <>
@@ -146,7 +204,7 @@ export default function Card({
         }}
       />
 
-      <GestureDetector gesture={verticalSwipe}>
+      <GestureDetector gesture={combinedGesture}>
         <Animated.View
           style={[
             {
@@ -172,12 +230,53 @@ export default function Card({
             }}
           />
 
+          {isTop && !bioExpanded && (
+            <>
+              <Animated.View
+                style={[
+                  { position: "absolute", top: 60, left: 30, zIndex: 40 },
+                  showHeartStyle,
+                ]}
+                pointerEvents="none"
+              >
+                <MaskedView
+                  style={{ transform: [{ rotateZ: "-20deg" }] }}
+                  maskElement={
+                    <AntDesign name="heart" size={100} color="white" />
+                  }
+                >
+                  <LinearGradient
+                    colors={["#2EB62C", "#C5E8B7"]}
+                    style={{ width: 100, height: 100 }}
+                  />
+                </MaskedView>
+              </Animated.View>
+
+              <Animated.View
+                style={[
+                  { position: "absolute", top: 55, right: 30, zIndex: 40 },
+                  showCrossStyle,
+                ]}
+                pointerEvents="none"
+              >
+                <MaskedView
+                  style={{ transform: [{ rotateZ: "20deg" }] }}
+                  maskElement={<Entypo name="cross" size={125} color="white" />}
+                >
+                  <LinearGradient
+                    colors={["#FD267D", "#FE6D58"]}
+                    style={{ width: 125, height: 125 }}
+                  />
+                </MaskedView>
+              </Animated.View>
+            </>
+          )}
+
           {!bioExpanded && (
             <View className="absolute top-4 w-full flex-row justify-center gap-3 px-4">
               {photos.map((_, index) => (
                 <View
                   key={index}
-                  style={{}}
                   className={`h-1.5 flex-1 rounded-full overflow-hidden ${
                     index === currentIndex ? "bg-white" : "bg-gray-500/80"
                   }`}
@@ -206,7 +305,7 @@ export default function Card({
                 <View className="flex-1 mr-4" pointerEvents="none">
                   <View className="flex-row items-baseline">
                     <Text className="text-white text-3xl font-bold">
-                      {item.full_name}{"  "}
+                      {item.full_name}{" "}
                     </Text>
                     <Text className="text-white text-2xl font-regular">
                       {item.age}
@@ -216,7 +315,7 @@ export default function Card({
                     className="text-white text-base font-regular mt-1"
                     numberOfLines={2}
                   >
-                    {item.bio?.replace(/\\n/g, "\n")}
+                    {item.bio?.replace(/\n/g, "\n")}
                   </Text>
                 </View>
 
@@ -267,62 +366,12 @@ export default function Card({
                   contentContainerStyle={{ paddingBottom: 80 }}
                 >
                   <Text className="text-white text-xl leading-8 font-regular">
-                    {item.bio?.replace(/\\n/g, "\n")}
+                    {item.bio?.replace(/\n/g, "\n")}
                   </Text>
                   <View className="h-20" />
                 </Animated.ScrollView>
               </View>
             </Animated.View>
-          )}
-
-          {isTop && !bioExpanded && (
-            <>
-              <TouchableOpacity
-                activeOpacity={0.7}
-                onPress={() => setReportMenuVisible(true)}
-                className="absolute top-10 right-4 z-50 bg-black/30 p-2.5 rounded-full border border-white/10"
-              >
-                <FontAwesome5 name="shield-alt" size={20} color="white" />
-              </TouchableOpacity>
-
-              <Animated.View
-                style={[
-                  { position: "absolute", top: 60, left: 30, zIndex: 40 },
-                  showHeartStyle,
-                ]}
-                pointerEvents="none"
-              >
-                <MaskedView
-                  style={{ transform: [{ rotateZ: "-20deg" }] }}
-                  maskElement={
-                    <AntDesign name="heart" size={100} color="white" />
-                  }
-                >
-                  <LinearGradient
-                    colors={["#2EB62C", "#C5E8B7"]}
-                    style={{ width: 100, height: 100 }}
-                  />
-                </MaskedView>
-              </Animated.View>
-
-              <Animated.View
-                style={[
-                  { position: "absolute", top: 55, right: 30, zIndex: 40 },
-                  showCrossStyle,
-                ]}
-                pointerEvents="none"
-              >
-                <MaskedView
-                  style={{ transform: [{ rotateZ: "20deg" }] }}
-                  maskElement={<Entypo name="cross" size={125} color="white" />}
-                >
-                  <LinearGradient
-                    colors={["#FD267D", "#FE6D58"]}
-                    style={{ width: 125, height: 125 }}
-                  />
-                </MaskedView>
-              </Animated.View>
-            </>
           )}
         </Animated.View>
       </GestureDetector>
